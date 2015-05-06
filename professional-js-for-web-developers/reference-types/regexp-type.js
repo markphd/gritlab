ae48cd35667560392b1190fdab1063979b44135d
Mark Philip Dawal <radical.mark@gmail.com>
2015-05-06 05:05:38 +0400

    Started RegExp Type

---
+++ professional-js-for-web-developers/reference-types/regexp-type.js
@@ -0,0 +1,57 @@
+// REGEXP
+/*
+* Match all instances of "at" in a string.
+*/
+var pattern1 = /at/g;
+
+/*
+*Match the first instance of "bat" or "cat", regardless of case.
+*/
+var pattern2 = /[bc]at/i;
+
+/*
+* Match all three-character combinations ending with "at", regardless of case.
+*/
+var pattern3 = /.at/gi;
+
+
+// Meta characters
+// ( [ { \ ^ $ | ) ] } ? * + .
+
+
+/*
+* Match the first instance of "bat" or "cat", regardless of case.
+*/
+var pattern1 = /[bc]at/i;
+
+/*
+* Match the first instance of "[bc]at", regardless of case.
+*/
+var pattern2 = /\[bc\]at/i;
+
+/*
+* Match all three-character combinations ending with "at", regardless of case.
+*/
+var pattern3 = /.at/gi;
+
+/*
+* Match all instances of ".at", regardless of case.
+*/
+var pattern4 = /\.at/gi;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
