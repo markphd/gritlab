ffa20e0948f0990ff86c0b5bfbae51cc2c5d5ee7
markphd <radical.mark@gmail.com>
2015-05-12 23:56:27 +0400

    Started Singleton Built-in Objects

---
+++ professional-js-for-web-developers/reference-types/singleton-built-in-objects.js
@@ -0,0 +1,5 @@
+////////////////////////////////
+// SINGLETON BUILT-IN OBJECTS //
+////////////////////////////////
+
+// ECMA-262 defines a built-in object as "any object supplied by an ECMAScript implementation, independent of the host environment, which is present at the start of the execution of an ECMAScript program." This means the developer does not need to explicitly instantiate a built-in object; it is already instantiated. 
