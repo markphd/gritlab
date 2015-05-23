012684179cb44708fe02d45a60e55f0e3bd035dc
markphd <radical.mark@gmail.com>
2015-05-23 04:10:58 +0400

    Looping - for and while Statements

---
+++ beginning-javascript/ch03-decisions-loops-functions/decisions-loops-functions.js
@@ -0,0 +1,158 @@
+// Chapter 3: Decisions, Loops, and Functions
+
+Validation of user input is probably one of the most common uses of decision making in JavaScript, but it's far from being the only use.
+In JavaScript, you can change the flow of the code's execution depending on whether a condition is true or false, using an if statement or a switch statement. 
+
+Precedence
+The == and != comparison operators have the lowest order of precedence, and the rest of the comparison operators, <, >, <=, and >=, have an equal precedence.
+
+var age = prompt("Enter age:", "");
+var isOverSixty = parseInt(age) > 60;
+document.write("Older than 60: " + isOverSixty);
+
+// The if Statement
+
+if (roomTemperature > 80)
+{
+   roomTemperature = roomTemperature - 10;
+   alert("It's getting hot in here");
+   alert("Air conditioning switched on");
+}
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+   <html xmlns="http://www.w3.org/1999/xhtml">
+   <body>
+
+   <script type="text/javascript">
+
+   var degFahren = Number(prompt("Enter the degrees Fahrenheit",32));
+   var degCent;
+
+   degCent = 5/9 * (degFahren - 32);
+
+   document.write(degFahren + "\xB0 Fahrenheit is " + degCent +
+      "\xB0 centigrade<br />");
+
+   if (degCent < 0)
+   {
+      document.write("That's below the freezing point of water");
+   }
+
+   if (degCent == 100)
+      document.write("That's the boiling point of water");
+
+   </script>
+
+   </body>
+   </html>
+
+   // There are no braces here, so if the condition is true, the only code to execute is the first line below the if statement. When you want to execute multiple lines in the case of the condition being true, braces are required.
+   // You saw that when degFahren is 32, degCent will be 0. So your if statement will be "Is 0 equal to 100?" The answer is clearly false, and the code won't execute. Again, when you set degFahren to 31, degCent will be calculated to be -0.55555555556; "Is −0.55555555556 equal to 100?" is also false, and the code won't execute.
+
+// Logical Operators
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+ "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<body>
+
+<script type="text/javascript">
+
+var myAge = Number(prompt("Enter your age",30));
+
+if (myAge >= 0 && myAge <= 10)
+{
+   document.write("myAge is between 0 and 10<br />");
+}
+
+if ( !(myAge >= 0 && myAge <= 10) )
+{
+   document.write("myAge is NOT between 0 and 10<br />");
+}
+
+if ( myAge >= 80 || myAge <= 10 )
+{
+   document.write("myAge is 80 or above OR 10 or below<br />");
+}
+
+if ( (myAge >= 30 && myAge <= 39) || (myAge >= 80 && myAge <= 89) )
+{
+   document.write("myAge is between 30 and 39 or myAge is between 80 and 89");
+}
+
+</script>
+
+</body>
+</html>
+
+
+// Switch Case
+    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+    <html xmlns="http://www.w3.org/1999/xhtml">
+    <body>
+
+    <script type="text/javascript">
+
+    var secretNumber = prompt("Pick a number between 1 and 5:", "");
+    secretNumber = parseInt(secretNumber);
+
+    switch (secretNumber)
+    {
+    case 1:
+       document.write("Too low!");
+       break;
+
+    case 2:
+       document.write("Too low!");
+       break;
+
+    case 3:
+       document.write("You guessed the secret number!");
+       break;
+
+    case 4:
+       document.write("Too high!");
+       break;
+
+    case 5:
+       document.write("Too high!");
+       break;
+
+    default:
+       document.write("You did not enter a number between 1 and 5.");
+       break;
+    }
+    document.write("<br />Execution continues here");
+
+    </script>
+
+    </body>
+    </html>
+
+
+
+// Executing the Same Code for Different Cases
+    switch (secretNumber)
+      {
+
+      case 1:
+      case 2:
+         document.write("Too low!");
+         break;
+
+
+      case 3:
+         document.write("You guessed the secret number!");
+         break;
+
+
+      case 4:
+      case 5:
+         document.write("Too high!");
+         break;    
+
+
