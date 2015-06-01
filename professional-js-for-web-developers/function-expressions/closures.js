a0c5a0d7cf9340fc943c9e35053dd13973d55b01
Mark Philip Dawal <radical.mark@gmail.com>
2015-06-01 18:17:28 +0400

    The this Object

--- professional-js-for-web-developers/function-expressions/closures.js
+++ professional-js-for-web-developers/function-expressions/closures.js
@@ -50,4 +50,74 @@ var result = compare(5, 10);
 // Once the function has completed, the local activation object is destroyed, leaving only the global scope in memory. 
 // Closures, however, behave differently.
 
+// A function that is defined inside another function adds the containing function's activation object 
+// into its scope chain. 
+// So, in createComparisonFunction(), the anonymous function's scope chain actually contains a 
+// reference to the activation object for createComparisonFunction().
+
+var compare = createComparisonFunction("name");
+var result = compare({ name: "Nicholas" }, { name: "Greg" });
+
+// Another interesting side effect is that the activation object from createComparisonFunction() cannot be destroyed once the function finishes executing, because a reference still exists in the anonymous function's scope chain. 
+// After createComparisonFunction() completes, the scope chain for its execution context is destroyed, 
+// but its activation object will remain in memory until the anonymous function is destroyed
+
+//create function
+var compareNames = createComparisonFunction("name");
+
+//call function
+var result = compareNames({ name: "Nicholas" }, { name: "Greg"});
+
+//dereference function - memory can now be reclaimed
+compareNames = null;
+
+// Here, the comparison function is created and stored in the variable compareNames. 
+// Setting compareNames equal to null dereferences the function and allows the garbage collection routine to clean it up.
+
+
+// Since closures carry with them the containing function's scope, they take up more memory than other functions. 
+// Overuse of closures can lead to excess memory consumption, so it's recommended you use them only when absolutely necessary. 
+
+
+// Closures and Variables
+// The closure always gets the last value of any variable from the containing function. 
+// Remember that the closure stores a reference to the entire variable object, not just to a particular variable. 
+
+function createFunctions(){
+	var result = new Array();
+	for (var i=0; i < 10; i++){
+		result[i] = function(){
+		return i;
+		};
+	}
+	return result;
+}
+
+// every function returns 10. Since each function has the createFunctions() activation object in its scope chain, they are all referring to the same variable, i. 
+// When createFunctions() finishes running, the value of i is 10, and since every function 
+// references the same variable object in which i exists, the value of i inside each function is 10.
+
+function createFunctions(){
+	var result = new Array();
+
+	for (var i=0; i < 10; i++){
+		result[i] = function(num){
+		return function(){
+			return num;
+			};
+		}(i);
+	}
+	return result;
+}
+
+// With this version of createFunctions(), each function returns a different number. 
+// Instead of assigning a closure directly into the array, an anonymous function is defined and called immediately. 
+// The anonymous function has one argument, num, which is the number that the result function should return. 
+// The variable i is passed in as an argument to the anonymous function. 
+// Since function arguments are passed by value, the current value of i is copied into the argument num.
+
+
+
+
+// The this Object
 
