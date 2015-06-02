14e186c417448f5c45f1fda13ffb7f9c45ecc005
Mark Philip Dawal <radical.mark@gmail.com>
2015-06-02 08:24:56 +0400

    Started Private Variables

---
+++ professional-js-for-web-developers/function-expressions/private-variables.js
@@ -0,0 +1,5 @@
+// PRIVATE VARIABLES
+// Strictly speaking, JavaScript has no concept of private members; all object properties are public. 
+// There is, however, a concept of private variables. 
+// Any variable defined inside a function is considered private since it is inaccessible outside that function. 
+
