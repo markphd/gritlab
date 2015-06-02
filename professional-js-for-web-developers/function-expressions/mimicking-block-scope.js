9251a2ebd4e2d4f56a7e9bf62a5be1ef7f58be51
Mark Philip Dawal <radical.mark@gmail.com>
2015-06-02 07:22:04 +0400

    Started Mimicking Block Scope

---
+++ professional-js-for-web-developers/function-expressions/mimicking-block-scope.js
@@ -0,0 +1,4 @@
+// MIMICKING BLOCK SCOPE
+// As mentioned previously, JavaScript has no concept of block-level scoping, meaning variables defined inside of block statements 
+// are actually created in the containing function, not within the statement.
+
