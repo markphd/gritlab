6d08d2c669d2e3e3fc5467ee66a16b6560dabe00
markphd <radical.mark@gmail.com>
2015-06-08 07:19:15 +0400

    Started Beginning JS Chapter 3 - Functions

---
+++ beginning-javascript/ch03-decisions-loops-functions/functions.js
@@ -0,0 +1,7 @@
+// Functions
+// A function is something that performs a particular task. 
+// Functions in JavaScript work a little like the function buttons on a pocket calculator: 
+// They encapsulate a block of code that performs a certain task.
+// The data that a function requires to be passed are known as its parameter(s).
+// A function can have zero or more parameters, though even if it has no parameters, 
+// you must still put the open and close parentheses after its name.
