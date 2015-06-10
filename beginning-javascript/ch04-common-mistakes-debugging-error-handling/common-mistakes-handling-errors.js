dbe0121afd182ec71b4c87d291ea5a200a7a91af
markphd <radical.mark@gmail.com>
2015-06-10 09:00:37 +0400

    Started Beginning JS Chapter 4 -  Common Mistakes, Debugging, and Error Handling

---
+++ beginning-javascript/ch04-common-mistakes-debugging-error-handling/common-mistakes-handling-errors.js
@@ -0,0 +1,12 @@
+// Some Common Mistakes
+
+// Undefined Variables
+// Whether or not you use the var keyword to declare a variable has a consequence of what scope the variable has; 
+// so it is always best to use the var keyword
+
+// Case Sensitivity
+
+var myName = "Jeremy";
+If (myName == "jeremy")
+	alert(myName.toUppercase());
+
