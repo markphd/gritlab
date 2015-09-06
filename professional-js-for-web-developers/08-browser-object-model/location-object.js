8cb2763174fd805ddc9603a9e06617b0d5be1148
markphd <radical.mark@gmail.com>
2015-09-06 11:07:41 +0400

    ● Professional JS - Chapter 8: The Location Object

---
+++ professional-js-for-web-developers/08-browser-object-model/location-object.js
@@ -0,0 +1,5 @@
+// ● THE LOCATION OBJECT
+// One of the most useful BOM objects is location, which provides information about the document that is currently loaded in the window, 
+// as well as general navigation functionality. The location object is unique in that it is a property of both window and document; 
+// both window.location and document.location point to the same object. 
+
