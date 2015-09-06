a812a3d71fc765805509813359143a7f2393536a
markphd <radical.mark@gmail.com>
2015-09-06 08:35:08 +0400

    Window Object Continuation

--- professional-js-for-web-developers/08-browser-object-model/window-object.js
+++ professional-js-for-web-developers/08-browser-object-model/window-object.js
@@ -89,3 +89,30 @@ var newValue = window.oldValue;
   </frameset>
 </html>
 
+
+// The top object always points to the very top (outermost) frame, which is the browser window itself. 
+// There is one final window object, called self, which always points to window. The two can, in fact, be used interchangeably
+
+
+
+// Window Position
+// The position of a window object may be determined and changed using various properties and methods.
+// The end result is that there's no accurate way to determine the size of the browser window itself, but it is possible to get the dimensions of the page viewport, as shown in the following example:
+
+
+
+// Navigating and Opening Windows
+// The window.open() method can be used both to navigate to a particular URL and to open a new browser window. This method accepts four arguments: the URL to load, the window target, a string of features, and a Boolean value indicating that the new page should take the place of the currently loaded page in the browser history. 
+
+
+
+// Popping Up Windows
+// When the second argument doesn't identify an existing window or frame, a new window or tab is created based on a string passed in as the third argument.
+
+// Additionally, browsers will allow the creation of pop-up windows only after a user action. 
+// A call to window.open() while a page is still being loaded, for instance, will not be executed and may cause an error to be displayed to the user. 
+// Pop-up windows may be opened based only on a click or a key press.
+
+
+
+
